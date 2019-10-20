95ec69158b2444ed205d06bb450a05af7459717d
Jayant <milind.ck.dev@gmail.com>
2019-10-20 22:55:13 +0530

    ApiDesign

--- src/index.js
+++ src/index.js
@@ -2,11 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NavBar from './components/NavBar';
+import Users from './components/Users';
+import Posts from './components/Posts';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {BrowserRouter, Route} from 'react-router-dom'
+
+
+ReactDOM.render(
+  <BrowserRouter>
+  <div className="App">
+    <NavBar></NavBar>
+    <Route exact path="/" component={App}/>
+
+    <Route path="/Users" component={Users}/>
+    <Route path="/Posts/:id/:name" component={Posts}/>
+  </div>
+  </BrowserRouter>
+
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
